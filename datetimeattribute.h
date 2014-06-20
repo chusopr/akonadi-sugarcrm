26c4019bc026e275686a387aeb2350ca8cba2383
Jesús Pérez (Chuso) <git@chuso.net>
2014-06-20 12:18:49 +0200

    Updated copyright notice.

--- datetimeattribute.h
+++ datetimeattribute.h
@@ -1,9 +1,21 @@
 /*
- * Copyright (C) 2013 Jesus Perez (a) Chuso <github at chuso dot net>
- * http://chuso.net
+ * Copyright (c) 2013, 2014 Jesús Pérez (a) Chuso <kde at chuso dot net>
  *
- * Resitributable under the terms of the Artistic License
- * http://opensource.org/licenses/artistic-license.php
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Library General Public License as published
+ * by the Free Software Foundation; either version 2 of the License or
+ * ( at your option ) version 3 or, at the discretion of KDE e.V.
+ * ( which shall act as a proxy as in section 14 of the GPLv3 ), any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
  */
 
 #ifndef DATETIMEATTRIBUTE_H
